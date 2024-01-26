873d737299027aae1d6e7c62b0b1e18a97d171ab
kabbo25 <kabbo25@gmail.com>
2024-01-27 04:16:22 +0600

    making basic middleware with two method

---
+++ week-3.1/basic-middleware.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+function UserMiddleware(req, res, next) {
+  const username = req.headers.username;
+  const password = req.headers.password;
+  if (username !== "kabbo" || password !== "kabbo") {
+    res.status(400).json({
+      msg: "invalid authetication",
+    });
+  } else {
+    next();
+  }
+}
+function KidneyMiddleware(req, res, next) {
+  const n = req.query.n;
+  if (n < 0 || n > 2) {
+    res.status(403).json({
+      msg: "invalid input",
+    });
+  } else {
+    next();
+  }
+}
+
+app.get("/health-checkup", UserMiddleware, KidneyMiddleware, (req, res) => {
+  res.json({
+    msg: "your health is healthy",
+  });
+});
+
+app.get("/kidney-checkup", KidneyMiddleware, (req, res) => {
+  res.json({
+    msg: "Your kidney is healthy",
+  });
+});
+
+app.listen(3000);
