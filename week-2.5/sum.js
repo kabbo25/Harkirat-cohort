f280cf65fa650fe3fbd48480334ce22db2b3f1eb
kabbo25 <kabbo25@gmail.com>
2024-01-26 19:02:05 +0600

    using basic sum function in backend

---
+++ week-2.5/sum.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+function sum(n) {
+  let ans = 0;
+  for (let i = 0; i <= n; ++i) {
+    ans += i;
+  }
+  return ans;
+}
+app.get("/", (req, res) => {
+  const n = req.query.n;
+  const ans = sum(n);
+  res.send(`you sum is ${ans}`);
+});
+
+app.listen(3000);
