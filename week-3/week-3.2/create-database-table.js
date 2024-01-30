e4035550d9ceacecd92afdd410a2bd5c92a8337d
kabbo25 <kabbo25@gmail.com>
2024-01-30 16:26:28 +0600

    check commit

--- week-3/week-3.2/create-database-table.js
+++ week-3/week-3.2/create-database-table.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
+
 mongoose.connect(process.env.MONGODB_URI);
+
 const Cat = mongoose.model("Cat", { name: String });
+
 const kitty = new Cat({ name: "ovi" });
+
 kitty.save().then(() => console.log("meow"));
